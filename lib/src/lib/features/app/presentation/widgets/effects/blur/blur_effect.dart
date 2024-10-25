52ed85bd97789594bfc2eb7992904298769f7e26
Franco <ff>
2024-10-25 14:31:56 -0300

    agregando spoiler media

--- lib/src/lib/features/app/presentation/widgets/effects/blur/blur_effect.dart
+++ lib/src/lib/features/app/presentation/widgets/effects/blur/blur_effect.dart
@@ -1,18 +1,64 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 
-class BlurEffect extends StatelessWidget {
+// class BlurEffect extends StatelessWidget {
+//   final bool blurear;
+//   final Widget? child;
+//   const BlurEffect({super.key, this.blurear = true, this.child});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     Widget child = this.child ?? const SizedBox();
+
+//     if (blurear) {
+//       child = ClipRRect(
+//         clipBehavior: Clip.antiAlias,
+//         child: BackdropFilter(
+//           filter: ImageFilter.blur(
+//             sigmaX: 20,
+//             sigmaY: 15,
+//           ),
+//           child: child,
+//         ),
+//       );
+//     }
+
+//     return child;
+//   }
+// }
+
+abstract class BlurEffect extends StatelessWidget {
+  const BlurEffect._({super.key});
+
+  const factory BlurEffect({
+    required bool blurear,
+    Widget? child,
+  }) = _BlurEffect;
+
+  const factory BlurEffect.builder({
+    required bool blurear,
+    required Widget Function(BlurController, Widget) builder,
+    Widget? child,
+  }) = _BlurEffectBuilder;
+}
+
+class _BlurEffect extends BlurEffect {
   final bool blurear;
   final Widget? child;
-  const BlurEffect({super.key, this.blurear = true, this.child});
-
+  const _BlurEffect({
+    required this.blurear,
+    this.child,
+  }) : super._();
   @override
   Widget build(BuildContext context) {
     Widget child = this.child ?? const SizedBox();
-
     if (blurear) {
       child = ClipRRect(
         clipBehavior: Clip.antiAlias,
@@ -25,7 +71,46 @@ class BlurEffect extends StatelessWidget {
         ),
       );
     }
-
     return child;
+  }
+}
+
+class _BlurEffectBuilder extends BlurEffect {
+  final Widget Function(BlurController controller, Widget child) builder;
+  final bool blurear;
+  final Widget? child;
+
+  const _BlurEffectBuilder({
+    required this.builder,
+    required this.blurear,
+    this.child,
+  }) : super._();
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => BlurController(),
+      builder: (context, child) {
+        return builder(
+          context.read(),
+          BlurEffect(
+            blurear: context.watch<BlurController>().blurear,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class BlurController extends ChangeNotifier {
+  bool blurear;
+  BlurController({
+    this.blurear = true,
+  });
+
+  void switchBlur() {
+    blurear = !blurear;
+
+    notifyListeners();
   }
 }
--- lib/src/lib/features/hilo/presentation/hilo_screen.dart
+++ lib/src/lib/features/hilo/presentation/hilo_screen.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/src/lib/features/app/presentation/widgets/effects/blur/blur_effect.dart';
 import 'package:blog_app/src/lib/features/categorias/presentation/subcategoria_tile.dart';
 import 'package:blog_app/src/lib/features/comentarios/domain/models/comentario.dart';
 import 'package:blog_app/src/lib/features/comentarios/domain/models/typedef.dart';
@@ -194,62 +195,58 @@ class MediaSpoileable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
-    //return BlurBuilder(
-    //   builder: (context, child, controller) {
-    //     return Stack(
-    //       children: [
-    //         child,
-    //         controller.blurear
-    //             ? Positioned.fill(
-    //                 child: Center(
-    //                   child: _VerContenidoButton(
-    //                     controller: controller,
-    //                   ),
-    //                 ),
-    //               )
-    //             : const SizedBox(),
-    //       ],
-    //     );
-    //   },
-    //   child: child,
-    // );
+    return BlurEffect.builder(
+      blurear: true,
+      builder: (controller, child) {
+        return Stack(
+          children: [
+            child,
+            controller.blurear
+                ? const Positioned.fill(
+                    child: Center(
+                      child: _VerContenidoButton(),
+                    ),
+                  )
+                : const SizedBox(),
+          ],
+        );
+      },
+      child: child,
+    );
   }
 }
 
-// class _VerContenidoButton extends StatelessWidget {
-//   final BlurController controller;
-//   const _VerContenidoButton({
-//     super.key,
-//     required this.controller,
-//   });
+class _VerContenidoButton extends StatelessWidget {
+  const _VerContenidoButton({
+    super.key,
+  });
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return OutlinedButton(
-//       onPressed: () => controller.switchBlur(),
-//       style: ButtonStyle(
-//         backgroundColor: WidgetStatePropertyAll(Colors.white.withOpacity(0.15)),
-//         side: const WidgetStatePropertyAll(
-//           BorderSide(width: 0.5, color: Colors.transparent),
-//         ),
-//         shape: const WidgetStatePropertyAll(
-//           RoundedRectangleBorder(
-//             borderRadius: BorderRadius.all(Radius.circular(30)),
-//           ),
-//         ),
-//         padding: const WidgetStatePropertyAll(
-//           EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-//         ),
-//       ),
-//       child: const Text(
-//         "Ver contenido",
-//         style: TextStyle(
-//           fontSize: 20,
-//           color: Colors.white,
-//           fontWeight: FontWeight.bold,
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () => context.read<BlurController>().switchBlur(),
+      style: ButtonStyle(
+        backgroundColor: WidgetStatePropertyAll(Colors.white.withOpacity(0.15)),
+        side: const WidgetStatePropertyAll(
+          BorderSide(width: 0.5, color: Colors.transparent),
+        ),
+        shape: const WidgetStatePropertyAll(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+          ),
+        ),
+        padding: const WidgetStatePropertyAll(
+          EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+        ),
+      ),
+      child: const Text(
+        "Ver contenido",
+        style: TextStyle(
+          fontSize: 20,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
